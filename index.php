1110294a6047e4ec3e2b893eec792f5a8ee41176
U-DESKTOP-L3FMDUM\ibanp <ibanpallano1997@gmail.com>
2019-08-13 22:12:30 -0400

    Coloque los titles en los inputs

--- index.php
+++ index.php
@@ -25,9 +25,9 @@ require_once 'backend.php';
     <article>
         <form action="index.php" id="form" method="POST">
             <h1>Formulario de contacto</h1>
-            <input type="text" id="name" name="name" placeholder="Nombre completo" autocomplete="off" value="<?php if(isset($_POST["name"])){ echo $name; } ?>"><br>
-            <input type="text" id="email" name="email" placeholder="Correo electr&oacute;nico" autocomplete="off" value="<?php if(isset($_POST["email"])){ echo $email; } ?>"><br>
-            <textarea name="messege" id="messege" placeholder="Comunicado" autocomplete="off" maxlength="65000"><?php if(isset($_POST["messege"])){ echo $messege; } ?></textarea><br>
+            <input type="text" id="name" name="name" placeholder="Nombre completo" title="Por favor, llena este campo" autocomplete="off" value="<?php if(isset($_POST["name"])){ echo $name; } ?>"><br>
+            <input type="text" id="email" name="email" placeholder="Correo electr&oacute;nico" title="Por favor, llena este campo" autocomplete="off" value="<?php if(isset($_POST["email"])){ echo $email; } ?>"><br>
+            <textarea name="messege" id="messege" placeholder="Comunicado" title="Por favor, llena este campo" autocomplete="off" maxlength="65000"><?php if(isset($_POST["messege"])){ echo $messege; } ?></textarea><br>
             <button type="submit" name="submit">Contactar</button>
             <!-- Mensaje de error -->
             <div class="danger">
